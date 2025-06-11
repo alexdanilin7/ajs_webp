2b02fbb2b1f288e25e7a8c9d8ddd12ad5a597d3d
andanilin <andanilin7@gmail.com>
2025-06-11 17:44:36 +0200

    webpack task correct

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,33 @@ module.exports = {
   },
   module: {
     rules: [
+       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
     ],
   },
   plugins: [
